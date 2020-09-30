4260f813a115a215596b855acb3d39d00bcb92b5
Artjom Löbsack <ceigh@pm.me>
2020-10-01 03:43:16 +0500

    feat(configs): add xmonad

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,47 @@
+-- IMPORTS
+import XMonad
+import XMonad.Hooks.DynamicLog -- for bar
+import XMonad.Util.SpawnOnce -- for start hook
+import XMonad.Layout.Spacing -- for layout spacing
+import XMonad.Hooks.EwmhDesktops -- for chromium fullscreen
+import XMonad.Layout.NoBorders -- to hide lonely window border
+
+-- COMMON
+myTerminal = "st"
+
+myBorderWidth = 5
+myNormalBorderColor = "#B2B2B2"
+myFocusedBorderColor = "#FF5454"
+
+myModMask = mod4Mask -- mod key
+
+-- HOOKS
+myStartupHook = do
+  spawnOnce "nitrogen --restore &"
+  spawnOnce "picom &"
+
+myLayoutHook =
+  smartBorders (
+    spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True $
+      layoutHook def
+  )
+
+myHandleEventHook = fullscreenEventHook
+
+-- SUMMARY
+myConfig = def {
+  terminal = myTerminal,
+  borderWidth = myBorderWidth,
+  normalBorderColor = myNormalBorderColor,
+  focusedBorderColor = myFocusedBorderColor,
+
+  -- keys
+  modMask = myModMask,
+
+  -- hooks
+  startupHook = myStartupHook,
+  layoutHook = myLayoutHook,
+  handleEventHook = myHandleEventHook
+}
+
+main = xmonad =<< xmobar myConfig
