a789fba0e6f08cf37fef21c70437e4ad887df0fa
Artjom Löbsack <ceigh@pm.me>
2020-10-09 22:12:19 +0500

    fix(scripts): remove software dirs

--- software/personal/replace.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-[ -z $1 ] && echo 'No expression to replace' && exit 1 || IF=$1
-[ -z $2 ] && OF='' || OF=$2
-
-rg -l $IF | xargs -d '\n' sed -i 's@'"$IF"'@'"$OF"'@g' && \
-  rg $OF
