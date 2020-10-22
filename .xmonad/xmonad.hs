f03c55b0dc04f474a9d2394a091c45c574147f7b
Artjom Löbsack <me@ceigh.com>
2020-10-22 18:56:08 +0500

    feat(xmonad): manage workspaces and autorun

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,13 @@
 -- IMPORTS
 import XMonad
 import XMonad.Util.SpawnOnce -- for start hook
+import XMonad.Actions.SpawnOn -- pin windows to workspaces
 import XMonad.Util.EZConfig -- to configure keys
 import XMonad.Hooks.DynamicLog -- for bar
 import XMonad.Hooks.EwmhDesktops -- for chromium fullscreen
 import XMonad.Layout.Spacing -- for layout spacing
 import XMonad.Layout.NoBorders -- to hide lonely window border
+import qualified XMonad.StackSet as W -- attach windows to workspaces
 
 -- CONST
 fontDefault = "'Iosevka Semibold-13'"
@@ -15,18 +17,31 @@ colorRed = "#FF5454"
 colorYellow = "#E3C78A"
 colorGreen = "#8CC85F"
 
-run_dmenu = "dmenu_run" ++
-  " -fn " ++ fontDefault ++
-  " -nf " ++ "'" ++ colorWhite ++ "'" ++
-  " -nb " ++ "'" ++ colorBlack ++ "'" ++
-  " -sb " ++ "'" ++ colorRed ++ "'"
-
 -- COMMON
+-- terminal
 myTerminal = "st"
 
+-- open terminal and run command
 term :: String -> String
 term command = myTerminal ++ " -e sh -c \"" ++ command ++ "\" &"
 
+-- commands
+runDmenu = "dmenu_run" ++
+  " -fn " ++ fontDefault ++
+  " -nf " ++ "'" ++ colorWhite ++ "'" ++
+  " -nb " ++ "'" ++ colorBlack ++ "'" ++
+  " -sb " ++ "'" ++ colorRed ++ "' &"
+runNewsboat = term "newsboat --refresh-on-start"
+runMutt     = term "mutt"
+
+-- workspaces
+ws1 = "1:WWW"
+ws2 = "2:ZSH"
+ws3 = "3:IRC"
+ws4 = "4:VID"
+myWorkspaces = [ws1, ws2, ws3, ws4] ++ map show [5..9]
+
+-- bar
 myBar = "xmobar"
 myPP = xmobarPP {
   ppCurrent = xmobarColor colorRed "" . wrap "<" ">",
@@ -35,6 +50,7 @@ myPP = xmobarPP {
   ppSep = " | "
 }
 
+-- border
 myBorderWidth = 5
 myNormalBorderColor = colorWhite
 myFocusedBorderColor = colorRed
@@ -50,6 +66,11 @@ myStartupHook = do
   spawnOnce "dunst &"
   spawnOnce "wallpaper-unsplash &"
   spawnOnce "nts run &"
+  spawnOnce "firefox &"
+  spawnOnce runNewsboat
+  spawnOnce runMutt
+  spawnOnOnce ws3 "ripcord &"
+  spawnOnOnce ws3 "telegram-desktop &"
 
 myLayoutHook =
   smartBorders (
@@ -59,32 +80,37 @@ myLayoutHook =
 
 myHandleEventHook = fullscreenEventHook
 
-myManageHook = composeAll
-  [
-    className =? "mpv" --> doFloat
-  ]
+myManageHook = manageSpawn <+> composeAll
+  [ className =? "mpv"              --> doFloat
+  , className =? "firefox"          --> moveTo ws1
+  , appName   =? "ripcord"          --> moveTo ws3
+  , appName   =? "telegram-desktop" --> moveTo ws3
+  ] where moveTo = doF . W.shift
 
 -- ADDITIONAL KEYS
 myAdditionalKeys =
   [
     -- dmenu
-    ((m, xK_p), spawn run_dmenu),
-
-    -- volume
-    ((m, xK_equal), spawn "pulsemixer --change-volume +10 &"),
-    ((m, xK_minus), spawn "pulsemixer --change-volume -10 &"),
-    ((m, xK_0), spawn "pulsemixer --toggle-mute &"),
+    ((m, xK_p), spawn runDmenu),
+    -- newsboat
+    ((m, xK_a), spawn runNewsboat),
+    -- firefox
+    ((m, xK_f), spawn "firefox &"),
+    -- ranger
+    ((m, xK_z), spawn (term "ranger")),
 
     -- play radio
     ((m, xK_r), spawn "nts run &"),
     ((m .|. shiftMask, xK_r), spawn "nts run 2 &"),
     ((m, xK_e), spawn "nts end &"),
 
-    -- firefox
-    ((m, xK_f), spawn "firefox &"),
+    -- volume
+    ((m, xK_equal), spawn "pulsemixer --change-volume +10 &"),
+    ((m, xK_minus), spawn "pulsemixer --change-volume -10 &"),
+    ((m, xK_0), spawn "pulsemixer --toggle-mute &"),
 
-    -- newsboat
-    ((m, xK_a), spawn (term "newsboat --refresh-on-start")),
+    -- lock screen
+    ((m .|. shiftMask, xK_l), spawn "xscreensaver-command -lock &"),
 
     -- power
     ((m, xK_End), spawn (term "sudo shutdown now")),
@@ -94,19 +120,14 @@ myAdditionalKeys =
     ((m, xK_Print),
       spawn "import -window root $HOME/pictures/`date +%d-%m-%H:%M`.png &"),
 
-    -- ranger
-    ((m, xK_z), spawn (term "ranger")),
-
     -- change wallpaper
-    ((m, xK_d), spawn "wallpaper-unsplash once &"),
-
-    -- lock screen
-    ((m .|. shiftMask, xK_l), spawn "xscreensaver-command -lock &")
+    ((m, xK_d), spawn "wallpaper-unsplash once &")
   ]
 
 -- SUMMARY
 myConfig = def {
   terminal = myTerminal,
+  workspaces = myWorkspaces,
   borderWidth = myBorderWidth,
   normalBorderColor = myNormalBorderColor,
   focusedBorderColor = myFocusedBorderColor,
