6a5309e193b1c79bfafb9de9f1617285a790a4aa
Artjom Löbsack <me@ceigh.com>
2021-04-12 22:32:08 +0500

    refactor(xmonad): reformat config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 -- IMPORTS
+-- xmonad
 import XMonad
 import XMonad.Util.SpawnOnce          -- start hook
 import XMonad.Actions.SpawnOn         -- pin windows to workspaces
@@ -11,24 +12,20 @@ import XMonad.Layout.NoBorders        -- hide lonely window border
 import XMonad.Hooks.EwmhDesktops      -- steam games
 import XMonad.Hooks.ManageHelpers     -- doFullFloat and other
 import qualified XMonad.StackSet as W -- attach windows to workspaces
+-- data
 import Data.Char (toUpper)            -- to uppercase strings
 
 -- CONST
+-- colors
 colorRed    = "#ac4142"
 colorYellow = "#e5b567"
 colorGreen  = "#b4c973"
 
--- COMMON
--- terminal
-myTerminal = "st"
-
--- open terminal and run command
-term :: String -> String
-term command = myTerminal ++ " -e sh -c \"" ++ command ++ "\" &"
-
--- commands
-runNewsboat = term "newsboat --refresh-on-start"
-runMutt     = term "neomutt"
+-- HELPERS
+-- count windows in workspace
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' .
+  W.stack . W.workspace . W.current . windowset
 
 -- make screenshot
 -- fullscreen: capture root, otherwise select area
@@ -46,21 +43,29 @@ shot fullscreen save
     clip   = "png:- | xclip -selection clipboard -target image/png"
     bg     = " &"
 
--- workspaces
-ws1 = "1"
-ws2 = "2"
-ws3 = "3"
-ws4 = "4"
-myWorkspaces = [ws1, ws2, ws3, ws4] ++ map show [5..9]
+-- open terminal and run command
+term :: String -> String
+term command = myTerminal ++ " -e sh -c \"" ++ command ++ "\" &"
 
-windowCount :: X (Maybe String)
-windowCount = gets $ Just . show . length . W.integrate' .
-  W.stack . W.workspace . W.current . windowset
+-- commands
+runNewsboat = term "newsboat --refresh-on-start"
+runMutt     = term "neomutt"
+
+-- COMMON
+-- terminal
+myTerminal = "st"
+
+-- workspaces
+ws1 = "WWW"
+ws2 = "DEV"
+ws3 = "IRC"
+ws4 = "RSS"
+myWorkspaces = [ws1, ws2, ws3, ws4] ++ map show [5..9]
 
 -- bar
 myBar = "xmobar"
 myPP  = xmobarPP
-  { ppCurrent = xmobarColor colorGreen  "" . wrap "<" ">"
+  { ppCurrent = xmobarColor colorGreen  ""
   , ppUrgent  = xmobarColor colorRed    "" . wrap "*" "*"
   , ppLayout  = take 128 . drop 8 . map toUpper
   , ppTitle   = shorten 32 . map toUpper
@@ -74,28 +79,27 @@ myBorderWidth        = 1
 myNormalBorderColor  = "#000"
 myFocusedBorderColor = "#575657"
 
--- KEYS
-m = mod4Mask
-myModMask = m
-myToggleStruts XConfig { XMonad.modMask = m } = (m, xK_b)
-
 -- HOOKS
+-- events
+myHandleEventHook = fullscreenEventHook
+
+-- startup
 myStartupHook = do
   spawnOnce       "wallpaper &"
   spawnOnce       "nts run &"
   spawnOnOnce ws3 "discord &"
   spawnOnOnce ws3 "telegram-desktop &"
 
+-- layout
 myLayoutHook = smartBorders $ spacingRaw
   False (Border 20 20 20 20) True (Border 10 10 10 10) True $
-  layoutTall ||| layoutMirrorTall ||| Grid ||| layoutSpiral ||| Full
+  layoutTall ||| layoutMirrorTall ||| Grid ||| Full
     where
       layoutTall       = Tall 1 (5 / 100) (2 / 3)
       layoutMirrorTall = Mirror (Tall 1 (5 / 100) (2 / 3))
-      layoutSpiral     = spiral (6 / 7)
+      -- layoutSpiral     = spiral (6 / 7)
 
-myHandleEventHook = fullscreenEventHook
-
+-- manage
 myManageHook = manageSpawn <+> composeAll
   [ className =? "mpv"              --> doFloat
   , title     =? "Media viewer"     --> doFullFloat  -- telegram media
@@ -104,14 +108,26 @@ myManageHook = manageSpawn <+> composeAll
   , appName   =? "telegram-desktop" --> moveTo ws3
   ] where moveTo = doF . W.shift
 
--- ADDITIONAL KEYS
+-- KEYS
+-- common
+m = mod4Mask
+myModMask = m
+myToggleStruts XConfig { XMonad.modMask = m } = (m, xK_b)
+
+-- additional
 myAdditionalKeys =
-  [ ((m, xK_p),               spawn "rofi -show run") -- rofi
+  [
+  -- rofi
+  ((m, xK_p),                 spawn "rofi -show run")
   , ((m .|. shiftMask, xK_p), spawn "rofi -show")
-  , ((m, xK_a),               spawn runNewsboat)      -- newsboat
-  , ((m, xK_f),               spawn "firefox &")      -- firefox
-  , ((m, xK_z),               spawn $ term "ranger")  -- ranger
-  , ((m .|. shiftMask, xK_m), spawn runMutt)          -- mutt
+
+  , ((m, xK_a),               spawn runNewsboat)
+
+  , ((m, xK_f),               spawn "firefox &")
+
+  , ((m, xK_z),               spawn $ term "ranger")
+
+  , ((m .|. shiftMask, xK_m), spawn runMutt)
 
   -- radio
   , ((m, xK_r),               spawn "nts run &")
@@ -124,7 +140,6 @@ myAdditionalKeys =
   , ((m, xK_0),     spawn "pulsemixer --toggle-mute &")
 
   -- lock screen
-  -- , ((m .|. shiftMask, xK_l), spawn "xscreensaver-command -lock &")
   , ((m .|. shiftMask, xK_l), spawn "slock")
 
   -- power
@@ -154,22 +169,17 @@ myAdditionalKeys =
   , ((m, xK_F12), spawn "natural-scrolling")
   ]
 
--- CONFIG
+-- SUMMARY
 myConfig = def
-  { terminal = myTerminal
-  , workspaces = myWorkspaces
-  , borderWidth = myBorderWidth
-  , normalBorderColor = myNormalBorderColor
+  { terminal           = myTerminal
+  , workspaces         = myWorkspaces
+  , borderWidth        = myBorderWidth
+  , normalBorderColor  = myNormalBorderColor
   , focusedBorderColor = myFocusedBorderColor
-
-  -- keys
-  , modMask = myModMask
-
-  -- hooks
-  , startupHook = myStartupHook
-  , layoutHook = myLayoutHook
-  , handleEventHook = myHandleEventHook
-  , manageHook = myManageHook
+  , modMask            = myModMask
+  , startupHook        = myStartupHook
+  , layoutHook         = myLayoutHook
+  , handleEventHook    = myHandleEventHook
+  , manageHook         = myManageHook
   } `additionalKeys` myAdditionalKeys
-
 main = xmonad =<< statusBar myBar myPP myToggleStruts (ewmh myConfig)
