de1cd28d5f9e1af90229c1be7c87b2e19c2cd12d
Artjom Löbsack <me@ceigh.com>
2021-04-24 21:42:07 +0500

    refactor(xmonad): rearrange code

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,9 @@
 -- IMPORTS
+import Data.Char
 import XMonad
 import XMonad.Actions.SpawnOn
 import XMonad.Util.SpawnOnce
-import XMonad.Util.Run                -- calcPrompt
+import XMonad.Util.Run
 import XMonad.Util.EZConfig           -- configure keys
 import XMonad.Layout.Spacing
 import XMonad.Layout.Accordion
@@ -17,7 +18,6 @@ import XMonad.Prompt.ConfirmPrompt
 import XMonad.Prompt.Input
 import XMonad.Prompt.Shell
 import qualified XMonad.StackSet as W -- attach windows to workspaces
-import Data.Char (toUpper, isSpace)
 
 -- CONST
 colorBlack  = "#000"
@@ -27,14 +27,12 @@ colorGreen  = "#b4c973"
 
 -- HELPERS
 -- count windows in workspace
-windowCount :: X (Maybe String)
 windowCount = gets $ Just . show . length . W.integrate' .
   W.stack . W.workspace . W.current . windowset
 
 -- make screenshot
 -- fullscreen: capture root, otherwise select area
 -- save: save image in filesystem, otherwise copy to clipboard
-shot :: Bool -> Bool -> X()
 shot fullscreen save
   | fullscreen     && save     = spawn $ root ++ file
   | fullscreen     && not save = spawn $ root ++ clip
@@ -47,30 +45,18 @@ shot fullscreen save
     clip   = "png:- | xclip -selection clipboard -target image/png"
 
 -- toggle fullscreen
-toggleFull :: X()
 toggleFull = sequence_
   [ sendMessage ToggleStruts
   , toggleScreenSpacingEnabled
   , toggleWindowSpacingEnabled
   ]
 
--- open terminal and run command
-run :: String -> X()
-run command = spawn $
-  myTerminal ++ " -e sh -c '" ++ command ++ "'"
+term = runInTerm ""
 
 -- COMMON
-myTerminal = "st"
-
--- workspaces
-ws1 = "WWW"
-ws2 = "DEV"
-ws3 = "IRC"
-ws4 = "RSS"
-myWorkspaces = [ws1, ws2, ws3, ws4] ++ map show [5..9]
+myWorkspaces = ["WWW", "DEV", "IRC", "RSS"] ++ map show [5..9]
 
 -- bar
-myBar = "xmobar"
 myPP  = xmobarPP
   { ppCurrent = xmobarColor colorGreen "" . wrap "<" ">"
   , ppUrgent  = xmobarColor colorRed   "" . wrap "*" "*"
@@ -97,19 +83,12 @@ myXPConfig = def
   , defaultPrompter   = map toUpper
   }
 
--- border
-myBorderWidth        = 1
-myNormalBorderColor  = colorBlack
-myFocusedBorderColor = "#555"
-
 -- HOOKS
-myHandleEventHook = fullscreenEventHook
-
 myStartupHook = do
-  spawnOnce       "wallpaper"
-  spawnOnce       "nts run"
-  spawnOnOnce ws3 "discord"
-  spawnOnOnce ws3 "telegram-desktop"
+  spawnOnce "wallpaper"
+  spawnOnce "nts run"
+  spawnOnOnce (myWorkspaces !! 2) "discord"
+  spawnOnOnce (myWorkspaces !! 2) "telegram-desktop"
 
 myLayoutHook = smartBorders $
   spacingRaw False border True border True $
@@ -121,9 +100,9 @@ myLayoutHook = smartBorders $
 myManageHook = manageSpawn <+> composeAll
   [ className =? "mpv"              --> doFullFloat
   , title     =? "Media viewer"     --> doFullFloat
-  , className =? "firefox"          --> moveTo ws1
-  , appName   =? "discord"          --> moveTo ws3
-  , appName   =? "telegram-desktop" --> moveTo ws3
+  , className =? "firefox"          --> moveTo (head myWorkspaces)
+  , appName   =? "discord"          --> moveTo (myWorkspaces !! 2)
+  , appName   =? "telegram-desktop" --> moveTo (myWorkspaces !! 2)
   ] where moveTo = doF . W.shift
 
 -- PROMPTS
@@ -137,25 +116,23 @@ calcPrompt' prompter = inputPrompt myXPConfig (trim prompter) ?+ calc
           "sh" ["-c", "bc <<< '" ++ expr ++ "'"] "")
 
 -- KEYS
--- common
 m = mod4Mask
-myModMask = m
+
 myToggleStruts XConfig { XMonad.modMask = m } = (m, xK_b)
 
--- additional
 myAdditionalKeys =
   [ ((m, xK_p),   shellPrompt myXPConfig)
   , ((m, xK_c),   calcPrompt)
-  , ((m, xK_v),   run "nvim")
-  , ((m, xK_a),   run "newsboat --refresh-on-start")
-  , ((m, xK_z),   run "ranger")
-  , ((m, xK_F9),  run "htop")
+  , ((m, xK_v),   term "nvim")
+  , ((m, xK_a),   term "newsboat")
+  , ((m, xK_z),   term "ranger")
+  , ((m, xK_F9),  term "htop")
   , ((m, xK_d),   spawn "wallpaper once")
   , ((m, xK_f),   spawn "firefox")
   , ((m, xK_F11), spawn "kb-layout")
   , ((m, xK_F12), spawn "natural-scrolling")
 
-  , ((m .|. shiftMask, xK_m), run "neomutt")
+  , ((m .|. shiftMask, xK_m), term "neomutt")
   , ((m .|. shiftMask, xK_l), spawn "xautolock -locknow")
   , ((m .|. shiftMask, xK_b), toggleFull)
 
@@ -183,15 +160,16 @@ myAdditionalKeys =
 
 -- SUMMARY
 myConfig = def
-  { terminal           = myTerminal
+  { terminal           = "st"
   , workspaces         = myWorkspaces
-  , borderWidth        = myBorderWidth
-  , normalBorderColor  = myNormalBorderColor
-  , focusedBorderColor = myFocusedBorderColor
-  , modMask            = myModMask
+  , borderWidth        = 1
+  , normalBorderColor  = colorBlack
+  , focusedBorderColor = "#555"
+  , modMask            = m
   , startupHook        = myStartupHook
   , layoutHook         = myLayoutHook
-  , handleEventHook    = myHandleEventHook
+  , handleEventHook    = fullscreenEventHook
   , manageHook         = myManageHook
   } `additionalKeys` myAdditionalKeys
-main = xmonad =<< statusBar myBar myPP myToggleStruts (ewmh myConfig)
+main = xmonad =<<
+  statusBar "xmobar" myPP myToggleStruts (ewmh myConfig)
